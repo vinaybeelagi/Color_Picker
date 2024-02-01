946bb7c5603290ecb13c4cec22bf9ccf947eb3c9
vinaybeelagi <vinaykumar.b@neoito.com>
2024-02-01 20:23:55 +0530

    updated export popup and display colors in csv,#,'' format

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const uploadedImage = document.getElementById('selectedImage');
 const addBtn = document.getElementById('add');
 const removeBtn = document.getElementById('remove');
 const paletteContainer = document.querySelector('.color-palette');
+const exportBtn =document.querySelector('.export-button');
 const image = document.getElementById('selectedImage');
 let palette = [];
 const eyeDropper = new EyeDropper();
@@ -46,3 +47,38 @@ removeBtn.addEventListener('click', () => {
         addBtn.disabled = false;
     }
 });
+function generateCSV() {
+    const capitalizedPalette = palette.map(color => color.toUpperCase().replace('#', ''));
+    return capitalizedPalette.join(',');
+}
+
+exportBtn.addEventListener('click', () => {
+    const csvContent = generateCSV();
+    const withHashContent = generateWithHash();
+    const arrayContent = generateArray();
+  
+    const popup = document.getElementById("myPopup");
+    popup.style.display = "block";
+  
+    const popupContent = document.querySelector(".popup-content");
+    popupContent.innerHTML = `
+      <span class="close">&times;</span>
+      <h2>Export Palette</h2>
+      <div id="csvFormat">- CSV<br>${csvContent}</div>
+      <div id="withHashFormat">- With #<br>${withHashContent}</div>
+      <div id="arrayFormat">- Array<br>${arrayContent}</div>
+    `;
+  
+    const closeBtn = document.querySelector(".close");
+    closeBtn.addEventListener("click", () => {
+      popup.style.display = "none";
+    });
+});
+
+function generateWithHash() {
+    return palette.map(color => color.toUpperCase()).join(',');
+}
+
+function generateArray() {
+    return "[" + palette.map(color => "'" + color.toUpperCase().replace('#', '') + "'").join(', ') + "]";
+}
