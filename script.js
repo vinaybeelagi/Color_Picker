e7065e9ede7840ba50e83714c50d1121ed5a342e
vinaybeelagi <vinaykumar.b@neoito.com>
2024-02-01 19:21:43 +0530

    updated upto  pick color store in color-palette

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-const browseButton = document.querySelector(".browse-button");
-const selectedImage = document.getElementById("selectedImage");
-const fileInput = document.getElementById("fileInput");
+const browseBtn = document.querySelector('.browse-button');
+const uploadedImage = document.getElementById('selectedImage');
+const addBtn = document.getElementById('add');
+const removeBtn = document.getElementById('remove');
+const paletteContainer = document.querySelector('.color-palette');
+const image = document.getElementById('selectedImage');
+let palette = [];
+const eyeDropper = new EyeDropper();
 
-browseButton.addEventListener("click", () => fileInput.click());
+browseBtn.addEventListener('click', () => {
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
 
-fileInput.addEventListener("change", handleImage);
+    fileInput.onchange = (e) => {
+        const file = e.target.files[0];
+        const imgURL = URL.createObjectURL(file);
+        uploadedImage.src = imgURL;
+    };
 
-function handleImage() {
-    const file = fileInput.files[0];
+    fileInput.click();
+});
 
-    if (file) {
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-            selectedImage.src = e.target.result;
-            extractPalette(selectedImage);
-        };
-
-        reader.readAsDataURL(file);
+addBtn.addEventListener('click', async () => {
+    if (palette.length >= 10) {
+        addBtn.disabled = true;
+        return;
     }
-}
+
+    const { sRGBHex } = await eyeDropper.open();
+    palette.push(sRGBHex);
+    const picker = document.createElement('div');
+    picker.classList.add('color-picker');
+    picker.style.backgroundColor = sRGBHex;
+    paletteContainer.appendChild(picker);
+
+    if (palette.length < 10) {
+        addBtn.disabled = false;
+    }
+});
+
+removeBtn.addEventListener('click', () => {
+    palette.pop();
+    paletteContainer.lastChild.remove();
+
+    if (palette.length < 10) {
+        addBtn.disabled = false;
+    }
+});
